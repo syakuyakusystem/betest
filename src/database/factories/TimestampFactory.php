ff57ad54fea75eb044838850db19e35435061b49
syakuyaku <30tinjuu8@gmai.com>
2024-06-29 21:45:56 +0900

    ダミーデータ実装

---
+++ src/database/factories/TimestampFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Timestamps;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TimestampFactory extends Factory
+{
+    protected $model = Timestamps::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+       //
+    }
+}
